ee193d47674a76e31c7b89b1e5727816cc758171
Rizwan Khan <rizwi23.rk@gmail.com>
2022-04-29 09:05:17 +0530

    Enable async clipboard API

--- panX.js
+++ panX.js
@@ -40,6 +40,7 @@ iframe.style.borderRadius = "8px";
 iframe.style.transition =
   "visibility 0.15s, right 0.15s, width 0.15s, opacity 0.15s ease-in-out";
 iframe.setAttribute("id", "panXPanel");
+iframe.setAttribute("allow", "clipboard-write"); // To enable async clipboard API, specifically, to write on clipboard, for copy to clipboard features
 iframe.style.boxShadow =
   "rgba(0, 0, 0, 0.22) 0px 25px 57px 0px, rgba(0, 0, 0, 0.18) 0px 4px 14px 0px";
 iframe.src = chrome.runtime.getURL("panelContent.html");
